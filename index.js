9b7897a77c5ab568da68575a015fbb31d6c4a34b
GogoWwz <wwz_zjut_web@163.com>
2018-12-19 21:28:16 +0800

    连接mlab

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
 const express = require('express')
 const path = require('path')
 const app = express()
+const mongoose = require('mongoose')
 
+mongoose.connect(`mongodb://admin:wwz1369577@ds139534.mlab.com:39534/sharebay`)
+let db = mongoose.connection
 const Router = require('./router/index.js')
 
-app.use('/api', Router)
-
 app.use('/index', express.static(path.join(__dirname, 'resource')))
+app.use('/api', Router)
+db.on('error', err => console.log(err))
+db.once('open', () => {
+    console.log("连接成功")
+});
 
 app.listen(3000, () => {
     console.log('server is running at port 3000')
--- router/index.js
+++ router/index.js
@@ -1,20 +1,16 @@
 const express = require('express')
-
 const Router = express.Router()
 
 Router.get('/', (req, res) => {
-    console.log(req, res)
     res.send('Got a GET request')
 })
 
-Router.get('/login', (req, res) => {
-    console.log(req, res)
+Router.post('/login', (req, res) => {
     let data = { code: 0, message: "login success" }
     res.json(data)
 })
 
 Router.get('/register', (req, res) => {
-    console.log(req, res)
     res.send('Got a register request')
 })
 
